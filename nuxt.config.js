0645ac4179af7275e31a2a9d4a8f0391d28cd16e
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-09-10 09:14:34 +0600

    3. Adding External Fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800&display=swap"}
     ]
   },
   /*

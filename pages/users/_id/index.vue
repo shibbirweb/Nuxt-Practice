98b5856bcc52d4a13c8a5cffd671a7135b4ecb75
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-09-10 07:34:41 +0600

    6. Validating Parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,13 @@
 <template>
     <h1>A single user, with ID: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+  export default {
+    validate(data){
+      //console.log(data)
+      return /^\d+$/.test(data.params.id)
+
+    }
+  }
+</script>

b307a27af8111bc4a50ffcab41e0d01c7e63292d
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-09-11 05:19:48 +0600

    6. Adding Static Assets - The Background Image

--- pages/index.vue
+++ pages/index.vue
@@ -34,6 +34,7 @@
     box-sizing: border-box;
     background-position: center;
     background-size: cover;
+    background-image: url('~assets/images/main-page-background.jpg');
   }
 
   .intro h1 {

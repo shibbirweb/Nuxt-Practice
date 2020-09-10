925c28ced8631af2432f16cead9bec3ae575c2aa
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-09-10 06:28:38 +0600

    6. Understanding the Folder Structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        MyNuxtPractice
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1>Hello, I'm a first Vue/Nuxt app!</h1>
   </div>
 </template>
 

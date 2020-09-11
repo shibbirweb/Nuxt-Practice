bf135129fe498152690be8f93f2d39fe0b0d2472
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-09-11 07:23:31 +0600

    7. Adding Async Data to a Single Post

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="single-post-page">
       <section class="post">
-        <h1 class="post-title">Title of the post</h1>
+        <h1 class="post-title">{{ loadedPost.title }}</h1>
         <div class="post-details">
           <div class="post-detail">
-            Last updated on XXX
+            Last updated on {{ loadedPost.updatedDate }}
           </div>
           <div class="post-detail">
-            Written by NAME
+            Written by {{loadedPost.author}}
           </div>
         </div>
         <p class="post-content">
-          Content of the post
+          {{ loadedPost.content}}
         </p>
       </section>
 
@@ -20,6 +20,27 @@
       </section>
     </div>
 </template>
+
+<script>
+  export default {
+    asyncData(context, callback){
+      console.log("async data for single post")
+      setTimeout(() => {
+        callback(null, {
+          loadedPost: {
+            id: '1',
+            author: 'Shibbir',
+            title: 'My awesome title (ID: '+ context.params.id + ')',
+            previewText: 'Super amazing, thanks for that!',
+            updatedDate: new Date,
+            content: 'Some dummy text which is definitely not the preview text though!',
+            thumbnail: 'https://images.idgesg.net/images/article/2019/05/cso_best_security_software_best_ideas_best_technology_lightbulb_on_horizon_of_circuit_board_landscape_with_abstract_digital_connective_technology_atmosphere_ideas_innovation_creativity_by_peshkov_gettyimages-965785212_3x2_2400x1600-100797318-large.jpg'
+          },
+        })
+      }, 1000)
+    }
+  }
+</script>
 
 <style scoped>
   .single-post-page {

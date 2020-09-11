bd9e95389c6889dfe04a7dab24aba4a1b3e3e121
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-09-11 09:48:49 +0600

    5. Fetching Data from the Backend

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex'
+import axios from 'axios'
 
 const createStore = () => {
   return new Vuex.Store({
@@ -12,30 +13,15 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            vuexContext.commit('setPosts', [
-              {
-                id: '1',
-                author: 'Shibbir',
-                title: 'My awesome title',
-                previewText: 'Super amazing, thanks for that!',
-                thumbnail: 'https://images.idgesg.net/images/article/2019/05/cso_best_security_software_best_ideas_best_technology_lightbulb_on_horizon_of_circuit_board_landscape_with_abstract_digital_connective_technology_atmosphere_ideas_innovation_creativity_by_peshkov_gettyimages-965785212_3x2_2400x1600-100797318-large.jpg'
-              },
-              {
-                id: '2',
-                author: 'Shibbir',
-                title: 'My second title',
-                previewText: 'Super amazing, thanks for that!',
-                thumbnail: 'https://images.idgesg.net/images/article/2019/05/cso_best_security_software_best_ideas_best_technology_lightbulb_on_horizon_of_circuit_board_landscape_with_abstract_digital_connective_technology_atmosphere_ideas_innovation_creativity_by_peshkov_gettyimages-965785212_3x2_2400x1600-100797318-large.jpg'
-              }
-            ])
-
-            resolve()
-
-
-          }, 1500)
-        })
+        return axios.get('https://nuxt-practice-b8878.firebaseio.com/posts.json')
+          .then(res => {
+            const postsArray = [];
+            for (const key in res.data){
+              postsArray.push({...res.data[key], id: key})
+            }
+            vuexContext.commit('setPosts', postsArray)
+          }).
+        catch(e => context.error(e));
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts)

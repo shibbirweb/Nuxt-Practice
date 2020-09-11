5beca66881810deb6e797732b0d4461a1b4f6735
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-09-11 12:38:01 +0600

    7. Adding Plugins

---
+++ plugins/core-components.js
@@ -0,0 +1,11 @@
+/*
+import Vue from 'vue'
+
+import AppButton from "~/components/UI/AppButton";
+import AppControlInput from "~/components/UI/AppControlInput";
+import PostList from "~/components/Posts/PostList";
+
+Vue.use('AppButton', AppButton)
+Vue.use('AppControlInput', AppControlInput)
+Vue.use('PostList', PostList)
+*/

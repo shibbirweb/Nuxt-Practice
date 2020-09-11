5fd7d0833908973686065967556d79ec6cf956eb
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-09-11 06:44:01 +0600

    2. Adding Dynamic Data

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,26 +1,15 @@
 <template>
   <section class="post-list">
     <PostPreview
-      id="1"
+      v-for="post in posts"
+      :key="post.id"
+      id="post.id"
       :is-admin="isAdmin"
-      thumbnail="https://images.idgesg.net/images/article/2019/05/cso_best_security_software_best_ideas_best_technology_lightbulb_on_horizon_of_circuit_board_landscape_with_abstract_digital_connective_technology_atmosphere_ideas_innovation_creativity_by_peshkov_gettyimages-965785212_3x2_2400x1600-100797318-large.jpg"
-      title="Hello there"
-      previewText="This is my first page"
+      :thumbnail="post.thumbnail"
+      :title="post.title"
+      :previewText="post.previewText"
     />
-    <PostPreview
-      id="2"
-      :is-admin="isAdmin"
-      thumbnail="https://images.idgesg.net/images/article/2019/05/cso_best_security_software_best_ideas_best_technology_lightbulb_on_horizon_of_circuit_board_landscape_with_abstract_digital_connective_technology_atmosphere_ideas_innovation_creativity_by_peshkov_gettyimages-965785212_3x2_2400x1600-100797318-large.jpg"
-      title="Hello there"
-      previewText="This is my second page"
-    />
-    <PostPreview
-      id="3"
-      :is-admin="isAdmin"
-      thumbnail="https://images.idgesg.net/images/article/2019/05/cso_best_security_software_best_ideas_best_technology_lightbulb_on_horizon_of_circuit_board_landscape_with_abstract_digital_connective_technology_atmosphere_ideas_innovation_creativity_by_peshkov_gettyimages-965785212_3x2_2400x1600-100797318-large.jpg"
-      title="Hello there"
-      previewText="This is my third page"
-    />
+
   </section>
 </template>
 
@@ -30,6 +19,10 @@
       isAdmin: {
         type: Boolean,
         default: false
+      },
+      posts: {
+        type: Array,
+        required: true
       }
     }
   }
--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,33 @@
       <h1>Get the latest tech news!</h1>
     </section>
 
-    <PostList/>
+    <PostList :posts="loadedPosts"/>
   </div>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        loadedPosts: [
+          {
+            id: '1',
+            author: 'Shibbir',
+            title: 'My awesome title',
+            previewText: 'Super amazing, thanks for that!',
+            thumbnail: 'https://images.idgesg.net/images/article/2019/05/cso_best_security_software_best_ideas_best_technology_lightbulb_on_horizon_of_circuit_board_landscape_with_abstract_digital_connective_technology_atmosphere_ideas_innovation_creativity_by_peshkov_gettyimages-965785212_3x2_2400x1600-100797318-large.jpg'
+          },{
+            id: '2',
+            author: 'Shibbir',
+            title: 'My second title',
+            previewText: 'Super amazing, thanks for that!',
+            thumbnail: 'https://images.idgesg.net/images/article/2019/05/cso_best_security_software_best_ideas_best_technology_lightbulb_on_horizon_of_circuit_board_landscape_with_abstract_digital_connective_technology_atmosphere_ideas_innovation_creativity_by_peshkov_gettyimages-965785212_3x2_2400x1600-100797318-large.jpg'
+          },
+        ]
+      }
+    }
+  }
+</script>
 
 <style scoped>
   .intro {

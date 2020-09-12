8bafc20592cfdc4101780e2145bba24da16a78cb
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-09-12 06:50:37 +0600

    3. Testing the Middleware

--- store/index.js
+++ store/index.js
@@ -74,6 +74,11 @@ const createStore = () => {
             Cookie.set('jwt', result.idToken)
             Cookie.set('expirationDate', new Date().getTime() + Number.parseInt(result.expiresIn) * 1000)
 
+
+            return this.$axios.$post('http://localhost:3000/api/track-data', {
+              data: 'authenticated'
+            })
+
           })
           .catch(e => console.log(e))
       },
